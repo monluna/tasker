43846d674cf7ee4e8dc96354e434da86e500eb18
Monluna <nastina2706@mail.ru>
2024-09-30 23:10:01 +0300

    Fix frontend tests

--- apps/frontend/src/components/header/Logo.tsx
+++ apps/frontend/src/components/header/Logo.tsx
@@ -3,7 +3,7 @@ import { StyleHeader__logo, StyleHeader__logo_div  } from "./style";
 export default function Header__Logo() {
   return (
     <StyleHeader__logo_div >
-      <StyleHeader__logo href="">Tasker</StyleHeader__logo>
+      <StyleHeader__logo href="" data-testid="header-logo-title">Tasker</StyleHeader__logo>
     </StyleHeader__logo_div >
   );
 }
